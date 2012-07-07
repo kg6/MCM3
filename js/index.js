6cd9c32687ad30d27e998c5d1959150d7968d8c5
Markus Zimmermann <markus.zimmermann@nethead.at>
2012-07-07 19:39:15 +0200

    draw dots in one run

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
-var canvas;
-var canvas_margin = { top: 10, right: 10, bottom: 10, left: 60 },
+var canvas,
+	canvas_margin = { top: 10, right: 10, bottom: 10, left: 60 },
 	canvas_width = 700 - canvas_margin.left - canvas_margin.right,
 	canvas_height = 600 - canvas_margin.top - canvas_margin.bottom;
 
@@ -36,13 +36,13 @@ function setup() {
 		requestRandomNumbers(samples_count, bound_x_min, bound_x_max, $('#use_random_org').is(':checked'), function(values) {
 			random_x_values = values;
 			
-			processMonteCarloMethod(func, random_x_values, random_y_values, drawApproximationPoint, displayResults);
+			processMonteCarloMethod(func, random_x_values, random_y_values, displayResults);
 		});
 
 		requestRandomNumbers(samples_count, bound_y_min, bound_y_max, $('#use_random_org').is(':checked'), function(values) {
 			random_y_values = values;
 			
-			processMonteCarloMethod(func, random_x_values, random_y_values, drawApproximationPoint, displayResults);
+			processMonteCarloMethod(func, random_x_values, random_y_values, displayResults);
 		});
 
 		// do not submit the form!
@@ -166,16 +166,6 @@ function displayResults(pos, neg, area) {
 		.append('<p><b class="area">Approximated area</b> (in the curve): ' + area + '</p>');
 }
 
-// draw one dot of the monte carlo method
-function drawApproximationPoint(x, y, dot_in) {
-	canvas.append('circle')
-		.attr('cx', diagram_scale_x(x))
-		.attr('cy', diagram_scale_y(y))
-		.attr('r', 3.0)
-		.attr('class', (dot_in) ? 'dot-in' : 'dot-out')
-		.attr('title', x + ', ' + y);
-}
-
 // fetch random numbers and execute the callback function if possible
 function requestRandomNumbers(number_count, normalize_min, normalize_max, use_random_org, callback) {
 	var normalize_range = (normalize_max - normalize_min);
@@ -219,7 +209,7 @@ function requestRandomNumbers(number_count, normalize_min, normalize_max, use_ra
 }
 
 // execute the monte carlo method
-function processMonteCarloMethod(func, random_x_values, random_y_values, datapoint_callback, result_callback) {
+function processMonteCarloMethod(func, random_x_values, random_y_values, result_callback) {
 	// check if the coordination data is already there
 	if (random_x_values == null || random_y_values == null)
 		return;
@@ -230,40 +220,35 @@ function processMonteCarloMethod(func, random_x_values, random_y_values, datapoi
 	
 	// go through every x-y-coordination data
 	var sample_count = Math.min(random_x_values.length, random_y_values.length);
+	var dots = [];
 	
 	for (var i = 0; i < sample_count; i++) {
 		// calculate the y function value of the x coordinate
 		var y = func(random_x_values[i]);
 		
 		// check if this dot is in positive area
-		if (y >= 0) { // y >= 0
-			// is this dot in the curve?
-			if (random_y_values[i] >= 0 && random_y_values[i] <= y) {
-				positive++;
-				
-				datapoint_callback(random_x_values[i], random_y_values[i], true);
-			}
-			else {
-				negative++;
-				
-				datapoint_callback(random_x_values[i], random_y_values[i], false);
-			}
+		if ((y >= 0 && random_y_values[i] >= 0 && random_y_values[i] <= y) || (y < 0 && random_y_values[i] <= 0 && random_y_values[i] >= y)) {
+			positive++;
+			
+			dots.push({ x: random_x_values[i], y: random_y_values[i], in: true });
 		}
-		else { // y < 0
-			// is this dot in the curve?
-			if (random_y_values[i] <= 0 && random_y_values[i] >= y) {
-				positive++;
-				
-				datapoint_callback(random_x_values[i], random_y_values[i], true);
-			}
-			else {
-				negative++;
-				
-				datapoint_callback(random_x_values[i], random_y_values[i], false);
-			}
+		else {
+			negative++;
+			
+			dots.push({ x: random_x_values[i], y: random_y_values[i], in: false });
 		}
-		
 	}
+	
+	// draw the dots
+	canvas.selectAll('circle')
+		.data(dots)
+		.enter()
+			.append('circle')
+				.attr('cx', function(d) { return diagram_scale_x(d.x); })
+				.attr('cy', function(d) { return diagram_scale_y(d.y); })
+				.attr('r', 3.0)
+				.attr('class', function(d) { return (d.in) ? 'dot-in' : 'dot-out' })
+				.attr('title', function(d) { return d.x + ', ' + d.y; });
 	
 	// calulate the percentage of positive and negative samples
 	positive /= sample_count;
